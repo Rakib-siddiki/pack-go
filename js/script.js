2d94ca61de4131606d5d9090c9f2f55ed87d7963
Rakib siddiki <rmrakib901@gmail.com>
2023-01-09 00:42:42 -0800

    init

---
+++ js/script.js
@@ -0,0 +1,126 @@
+$('.slider_part').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    centerMode: true,
+    centerPadding: '350px',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
+$('.expolore_sider').slick({
+    dots:true,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    centerMode:true,
+    centerPadding: 0,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
+$('.testomonial_slider').slick({
+    dots:true,
+    arrows: false,
+    
+    speed: 300,
+
+    slidesToShow: 1,
+    slidesToScroll: 1,
+   
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
+// mixitup
+var mixer = mixitup('.mixit_up_main');
+
+// cunter part 
+$('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
